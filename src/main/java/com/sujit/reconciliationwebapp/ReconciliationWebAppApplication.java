a63b32813b6c3c53510fac17f5ff814b2aa70ac6
sujit <sujit.sharma@clusus.com>
2021-03-10 14:47:58 +0545

    Initial Commit

---
+++ src/main/java/com/sujit/reconciliationwebapp/ReconciliationWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sujit.reconciliationwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconciliationWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconciliationWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujit/reconciliationwebapp/ReconciliationWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.reconciliationwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconciliationWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
