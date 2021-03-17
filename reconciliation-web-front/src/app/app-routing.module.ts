14a0259899523ecc181a885e042768a57038c3c0
sujit <sujit.sharma@clusus.com>
2021-03-17 18:06:06 +0545

    Implemented auth guard at frontend

--- reconciliation-web-front/src/app/app-routing.module.ts
+++ reconciliation-web-front/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {SourceComponent} from "./source/source.component";
+import {AuthGuard} from "./services/auth.guard";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent},
-  { path: 'sourceFile', component: SourceComponent }
+  { path: 'sourceFile', component: SourceComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
