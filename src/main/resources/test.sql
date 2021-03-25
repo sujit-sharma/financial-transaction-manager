80e3b435f5b7551d90867e20281efd6616e2ee02
sujit <sujit.sharma@clusus.com>
2021-03-25 07:48:42 +0545

    Test SQL file regenerated

---
+++ src/main/resources/test.sql
@@ -0,0 +1,10 @@
+select * from file_info;
+
+create table testing(
+                        name varchar(255),
+                        email varchar(255)
+);
+
+insert into testing (name, email) values('sujit', 'sujit@gmail.com');
+
+select * from user_entity;
