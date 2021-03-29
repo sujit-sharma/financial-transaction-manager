9bef601524b5d6eeb5778df0146b78b9235c4a96
sujit <sujit.sharma@clusus.com>
2021-03-29 09:07:53 +0545

    Transaction displayed in tab

--- reconciliation-web-front/src/app/result/result.component.ts
+++ reconciliation-web-front/src/app/result/result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatchEntity} from "../services/entities";
+import {FileHandleService} from "../services/file-handle.service";
 
 @Component({
   selector: 'app-result',
@@ -12,11 +13,14 @@ export class ResultComponent implements OnInit {
     { sn: 1, transactionId:'TR-47884222201',amount: 140.0,currency:'USD',valueDate:'2020-01-20' },
     { sn: 2, transactionId:'TR-47884222203',amount: 5000.0,currency:'JOD',valueDate: '2020-01-25' }
   ]
-  displayColumns: string[] = ['#', 'Transaction ID', 'Amount', 'Value Date'];
-  dataSource = this.MATCHING_DATA;
-  constructor() { }
+  displayColumns: string[] = ['transactionId', 'amount', 'currencyCode', 'date'];
+
+  constructor(private fileHandleService: FileHandleService) { }
 
   ngOnInit(): void {
   }
 
+  matchDataSource = this.fileHandleService.comparisonResult.MATCHING;
+  mismatchingDataSource = this.fileHandleService.comparisonResult.MISMATCHING;
+  missingDataSource = this.fileHandleService.comparisonResult.MISSING;
 }
