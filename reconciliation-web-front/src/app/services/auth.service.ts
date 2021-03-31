7b60e3b67cd679b5f098d922b478e66c57df33ee
sujit <sujit.sharma@clusus.com>
2021-03-31 18:52:55 +0545

    jwt token decode implemented

--- reconciliation-web-front/src/app/services/auth.service.ts
+++ reconciliation-web-front/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Router} from "@angular/router";
 
+import jwt_decode from 'jwt-decode';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -20,7 +22,31 @@ export class AuthService {
     this.router.navigate(['/login']);
   }
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('authToken');
+    // return !!localStorage.getItem('authToken');
+    const val = this.isTokenValid();
+    return val;
 
   }
+
+  private jwtDecode(token: string): object {
+    return jwt_decode(token);
+  }
+
+
+  private isTokenValid(): boolean {
+    let token = localStorage.getItem('authToken');
+    if(token){
+      const decodedToken:object = this.jwtDecode(token);
+      const expiryTime: number = this.getTokenExpireTime(decodedToken);
+      return ((1000 * expiryTime) - (new Date()).getTime()) > 5000;
+    }
+    console.log('cannot find token at local storage ');
+    return false;
+  }
+
+  private getTokenExpireTime(decodedToken: object): number {
+    // @ts-ignore
+    return decodedToken.exp;
+  }
+
 }
