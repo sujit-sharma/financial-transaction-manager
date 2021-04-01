29d2a2206b3e5136eedf6740a5f56f97ebe4130f
sujit <sujit.sharma@clusus.com>
2021-04-01 15:55:22 +0545

    Fix activity for login

--- src/main/java/com/sujit/reconciliationwebapp/controller/AuthController.java
+++ src/main/java/com/sujit/reconciliationwebapp/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.sujit.reconciliationwebapp.controller;
 import com.sujit.reconciliationwebapp.dto.LoginDto;
 import com.sujit.reconciliationwebapp.dto.LoginResponse;
 import com.sujit.reconciliationwebapp.model.UserActivity;
+import com.sujit.reconciliationwebapp.model.UserEntity;
+import com.sujit.reconciliationwebapp.repository.UserActivityRepository;
 import com.sujit.reconciliationwebapp.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,21 +15,24 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManager;
     private final TokenProvider tokenProvider;
+    private final UserActivityRepository userActivityRepository;
 
-    @GetMapping("user/login")
-    public ResponseEntity<Object> login(@RequestParam String username, @RequestParam String password) {
+    @GetMapping("/login")
+    public ResponseEntity<Object> login(@RequestParam String username, @RequestParam String password, HttpServletRequest request) {
         LoginDto loginDto = new LoginDto();
         loginDto.setUsername(username);
         loginDto.setPassword(password);
@@ -48,7 +53,12 @@ public class AuthController {
             response.setMessage("Login successful");
             //setting user Activity
             UserActivity userActivity = new UserActivity();
-            userActivity.setUsername(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+            User user  = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            userActivity.setUsername(user.getUsername());
+            userActivity.setActivity("login");
+            userActivity.setDate(LocalDateTime.now());
+            userActivity.setRequestIpAddr(request.getRemoteAddr());
+            userActivityRepository.save(userActivity);
         } catch (AuthenticationException e) {
             log.info("Invalid user/ password . Sign in Unsuccessful");
             response.setMessage("Invalid username or password");
@@ -56,6 +66,17 @@ public class AuthController {
         }
         log.info("User login success");
         return ResponseEntity.ok(response);
+
+    }
+
+    @GetMapping("/activity/{username}")
+    public ResponseEntity<Object> publishUserActivity(@PathVariable String username){
+        if(username == null){
+            return ResponseEntity.badRequest().build();
+        }
+        List<UserActivity> userActivity = userActivityRepository.findAllByUsername(username);
+        return ResponseEntity.ok().body(userActivity);
+
     }
 
 }
--- src/main/java/com/sujit/reconciliationwebapp/repository/UserActivityRepository.java
+++ src/main/java/com/sujit/reconciliationwebapp/repository/UserActivityRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserActivityRepository extends JpaRepository<UserActivity, Long> {
 
     @Query("Select ua from UserActivity ua where ua.username = ?1")
-    Optional<UserActivity> findUserActivityByUsername(String username);
+    UserActivity findFirstByUsername(String username);
+    List<UserActivity> findAllByUsername(String username);
 
 }
